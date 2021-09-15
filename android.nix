44447b8b7f3259ecb7b068149edafbb5005fe564
Mazurel <mateusz.mazur@yahoo.com>
2021-09-15 17:48:02 +0200

    Improved Android definition and removed default.nix

--- android.nix
+++ android.nix
@@ -1,16 +1,2 @@
-{ androidenv, ... }:
-androidenv.composeAndroidPackages {
-  toolsVersion = "26.1.1";
-  platformToolsVersion = "31.0.3";
-  buildToolsVersions = [ "28.0.3" "30.0.3" ];
-  platformVersions = [ "30" ];
-  abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-  cmakeVersions = [ "3.6.4111459" ];
-  includeNDK = true;
-  ndkVersions = [ "21.3.6528147" ];
-  includeEmulator = false;
-  includeSources = false;
-  includeSystemImages = false;
-  useGoogleAPIs = false;
-  useGoogleTVAddOns = false;
-}
+{ androidenv, lib, ... }:
+androidenv.composeAndroidPackages (lib.importTOML ./devshell.toml).android
